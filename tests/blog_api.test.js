e0c030657dbcac5f5eece9a063be111fbb176016
Thanatos <Thanatosh@gmail.com>
2024-03-12 18:05:50 +0200

    Part 4.10 Full Commit

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,11 +1,22 @@
-const { test, after, describe } = require('node:test')
+const { test, after, describe, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const assert = require('node:assert')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 describe('Backend tests', () => {
+
+  const initializeDatabase = async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(helper.initialBlogs)
+  }
+
+  beforeEach(async () => {
+    await initializeDatabase()
+  })
 
   test('Blogs are returned as json', async () => {
     await api
@@ -14,7 +25,7 @@ describe('Backend tests', () => {
       .expect('Content-Type', /application\/json/)
   })
 
-  test('There are two blogs', async () => {
+  test('There are 2 blogs', async () => {
     const response = await api.get('/api/blogs')
     assert.strictEqual(response.body.length, 2)
   })
@@ -31,8 +42,32 @@ describe('Backend tests', () => {
     const allHaveId = identification.every(hasId => hasId)
     assert.strictEqual(allHaveId, true)
   })
-})
 
+  test('Posting a new blog', async () => {
+    const newBlog = {
+      title: "Test Posting Blog",
+      author: "Test Person mk.III",
+      url: "postTestURL",
+      likes: 23,
+    }
+
+    const original = await api.get('/api/blogs')
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+
+    assert.strictEqual(blogs.length, original.body.length + 1)
+    
+    const newBlogInResponse = blogs.find(blog => blog.title === newBlog.title)
+    assert(newBlogInResponse !== undefined)
+  })
+})
   
 after(async () => {
   await mongoose.connection.close()
---
+++ tests/test_helper.js
@@ -0,0 +1,18 @@
+const initialBlogs = [
+  {
+    title: "Test Blog",
+    author: "Test Person",
+    url: "testURL",
+    likes: 0,
+  },
+  {
+    title: "Another Blog",
+    author: "Another Person",
+    url: "secondTestURL",
+    likes: 3,
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
