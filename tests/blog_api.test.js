ff7e7f244c6438131feb471267b0e14a968e8a63
Thanatos <Thanatosh@gmail.com>
2024-03-12 10:02:25 +0200

    Part 4.9 Full Commit

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -16,17 +16,23 @@ describe('Backend tests', () => {
 
   test('There are two blogs', async () => {
     const response = await api.get('/api/blogs')
-
     assert.strictEqual(response.body.length, 2)
   })
 
   test('The first blog is a Test Blog', async () => {
     const response = await api.get('/api/blogs')
-
     const titles = response.body.map(e => e.title)
     assert.strictEqual(titles.includes('Test Blog'), true)
   })
+
+  test('Blog identification is id on all blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const identification = response.body.map(blog => 'id' in blog)
+    const allHaveId = identification.every(hasId => hasId)
+    assert.strictEqual(allHaveId, true)
+  })
 })
+
   
 after(async () => {
   await mongoose.connection.close()
